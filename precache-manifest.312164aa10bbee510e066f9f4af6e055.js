d1d80b44cb88cba58c868dd40c2a63413412425c
Александр Долганов <dalex3734@yandex.ru>
2020-08-23 14:43:07 +0300

    Updates

---
+++ precache-manifest.312164aa10bbee510e066f9f4af6e055.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a77defa25916cc432f80f6414ea4e02d",
+    "url": "/invoice/index.html"
+  },
+  {
+    "revision": "b7357870a0db59ecbfea",
+    "url": "/invoice/static/css/main.a7a6bfb1.chunk.css"
+  },
+  {
+    "revision": "d129ff2a9eabd0e6f93f",
+    "url": "/invoice/static/js/2.78e3d704.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/invoice/static/js/2.78e3d704.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7357870a0db59ecbfea",
+    "url": "/invoice/static/js/main.22301e5f.chunk.js"
+  },
+  {
+    "revision": "aad0b71c0c9158291b35",
+    "url": "/invoice/static/js/runtime-main.beffbf50.js"
+  },
+  {
+    "revision": "07f65e84f390a81d91a50f5dd729f765",
+    "url": "/invoice/static/media/404error.07f65e84.jpg"
+  },
+  {
+    "revision": "44e643bc1aad6ad9674cf38faccf198d",
+    "url": "/invoice/static/media/avatar.44e643bc.jpg"
+  },
+  {
+    "revision": "a3dba4f731e55f772d6e25e8757f513f",
+    "url": "/invoice/static/media/icon-delete.a3dba4f7.svg"
+  }
+]);
